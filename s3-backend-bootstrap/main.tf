8bca22bda2d3fdd0e73784138c72996f3c15e489
Amparo Lucia Villalobos Ospina <226887593+luciaV1982@users.noreply.github.com>
2025-11-13 20:46:24 -0300

    Remove public access policy for S3 bucket
    Removed IAM policy document allowing public access to S3 state file and bucket listing. Updated bucket policy settings to restrict public access.

--- s3-backend-bootstrap/main.tf
+++ s3-backend-bootstrap/main.tf
@@ -7,65 +7,22 @@ locals {
   bucket_name = var.bucket_name
 }
 
-data "aws_iam_policy_document" "s3_backend_access" {
-  statement {
-    sid    = "AllowAnyoneAccessToStateFile"
-    effect = "Allow"
-
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-
-    }
-
-    actions = [
-      "s3:GetObject",
-      "s3:PutObject",
-      "s3:DeleteObject"
-    ]
-
-    resources = [
-      "arn:aws:s3:::${local.bucket_name}/global/cheese/terraform.tfstate"
-    ]
-  }
-
-  statement {
-    sid    = "AllowAnyoneListBucket"
-    effect = "Allow"
-
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
-
-    actions = ["s3:ListBucket"]
-
-    resources = ["arn:aws:s3:::${local.bucket_name}"]
-
-    condition {
-      test     = "StringLike"
-      variable = "s3:prefix"
-      values   = ["global/cheese/*"]
-    }
-  }
-}
-
 module "tfstate_bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "~> 4.1"
 
   bucket = local.bucket_name
-  policy = data.aws_iam_policy_document.s3_backend_access.json
 
   control_object_ownership = true
   object_ownership         = "BucketOwnerEnforced"
   acl                      = null
 
-  force_destroy               = false
-  restrict_public_buckets     = true
-  block_public_acls           = true
-  block_public_policy         = false
-  ignore_public_acls          = true
+  force_destroy = false
+
+  restrict_public_buckets = true
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
 
   versioning = {
     enabled = true
